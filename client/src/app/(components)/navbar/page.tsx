b20cad22b49f0679808095843af9788fc39d9865
chandima2000 <hychandima2000@gmail.com>
2025-05-06 10:13:57 +0530

    completed Darkmode functionality

--- client/src/app/(components)/navbar/page.tsx
+++ client/src/app/(components)/navbar/page.tsx
@@ -1,20 +1,28 @@
 'use client';
 
-import { setIsSidebarCollapsed } from '@/redux/slices/globalSlice';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/redux/slices/globalSlice';
 import { useAppDispatch, useAppSelector } from '@/redux/store/store';
-import { Menu, Search, Sun, Bell, Settings } from 'lucide-react';
+import { Menu, Search, Sun, Bell, Settings, Moon } from 'lucide-react';
 import Link from 'next/link';
 
 const Navbar = () => {
 
   const dispatch = useAppDispatch();
 
+  {/* sidebar collapsed */ }
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed
   );
 
   const toggleSidebar = () => {
     dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  }
+
+  {/* dark mode */ }
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode))
   }
 
 
@@ -35,7 +43,7 @@ const Navbar = () => {
           <input
             type="search"
             placeholder="search products"
-            className='pl-10 pr-4 py-2 w-50 md:w-80 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
+            className='pl-10 pr-4 py-2 w-50 md:w-60 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
           />
 
           <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
@@ -47,11 +55,18 @@ const Navbar = () => {
       {/* Right Side */}
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
+
+          {/* Dark mode function */}
           <div className="">
-            <button onClick={() => { }}>
-              <Sun className='cursor-pointer text-gray-500' size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className='cursor-pointer text-gray-500' size={24} />
+              ) : (
+                <Moon className='cursor-pointer text-gray-500' size={24} />
+              )}
             </button>
           </div>
+
           <div className="relative">
             <Bell className="cursor-pointer text-gray-500" size={24} />
             <span
--- client/src/app/dashboardWrapper/page.tsx
+++ client/src/app/dashboardWrapper/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../(components)/navbar/page";
 import Sidebar from "../(components)/sidebar/page";
 import { useAppSelector } from "@/redux/store/store";
@@ -11,8 +11,24 @@ const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed);
 
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+
+  //  This effect runs on each render.
+  //  It adds a dark or light class to the <html> tag.
+  //  This allows Tailwind CSS to apply the correct theme
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  }, [isDarkMode]);
+
+
   return (
-    <div>
+    <div className={`${isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       <Sidebar />
       <main
         className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
