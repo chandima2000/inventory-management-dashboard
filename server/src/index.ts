7c400c563c397ef736f99d4523ee722f1c25ea14
chandimaun4cus <hychandima2000@gmail.com>
2025-08-02 23:54:26 +0530

    Add aws configurations for backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,8 +37,8 @@ app.use("/users",userRoutes);
 app.use("/expenses", expenseRoutes);
 
 // Server Setup
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port,  () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server is running on port ${port}`);
 });
